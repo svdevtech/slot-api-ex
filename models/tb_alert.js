3cdf738273e7350505a7fbdc37daa630d17f3640
svdevtech <svdevtech@gmail.com>
2020-07-12 08:48:09 +0700

    add before pull

--- models/tb_alert.js
+++ models/tb_alert.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema
 const tb_alert_Schema = new Schema({
   alert_date: String,
   alert_date_end: String,
-  alert_detail:String,
-  alert_type:String
+  alert_detail: String,
+  alert_type: String
 })
 
 const Tb_AlertModel = mongoose.model('tb_alert', tb_alert_Schema)
